2043902ab14a5692241b73d6e62056e487feabd7
PyG4ng <91914909+PyG4ng@users.noreply.github.com>
2021-12-22 21:19:02 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ $(document).on("keypress", () => {
     }
 })
 
-$(document).on("click", () => {
+document.querySelector("body").addEventListener("click", () => {
     if (level === 0) {
         nextSequence();
     }
